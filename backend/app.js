1f7f1ba4c61b9ae3ff1635189b545016d010295a
Adeoluwa Idowu <adeoluwa53@gmail.com>
2023-07-27 00:31:59 +0100

    article routes

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,27 @@
 
+// import dependencies
 const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 
-//initializing express
+
+// importing custom modules
+const articleRoutes = require('./routes/articleRoutes')
+
+// initializing express
 const app = express()
+
 
 // configure dotenv
 dotenv.config()
+
+
+// middleware
+app.use(express.json())
+
+// to respond to the requests from the client
+app.use('/api/articles', articleRoutes)
+
 
 // connecting to mongoDB Atlas
 mongoose.connect(process.env.MONGO_URI,)
@@ -19,6 +33,7 @@ mongoose.connect(process.env.MONGO_URI,)
   .catch((err) => {
     console.log(err)
   })
+
 
 app.get('/', (req, res) => {
   res.json({mssg: 'Hello from backend'})
--- backend/routes/articleRoutes.js
+++ backend/routes/articleRoutes.js
@@ -4,5 +4,27 @@ const router = express.Router()
 
 // create an article
 router.post('/', (req, res) => {
-  res.json({mssg:'POST request'})
+  res.json({mssg:'request to POST'})
 })
+
+// get all articles 
+router.get('/',(req, res) => {
+  res.json({mssg:'request to GET ALL'})
+})
+
+// get one article
+router.get('/:id', (req, res) => {
+  res.json({mssg: 'request to GET ONE'})
+})
+
+// update one article
+router.patch('/:id', (req, res) => {
+  res.json({mssg: 'request to UPDATE ONE'})
+})
+
+// delete one article
+router.delete('/:id', (req, res) => {
+  res.json({mssg: 'request to DELETE ONE'})
+})
+
+module.exports = router

b1987e726d5daaa8d5746efc59189b4ef359682e
Adeoluwa Idowu <adeoluwa53@gmail.com>
2023-07-27 01:19:13 +0100

    articles Schema

--- backend/models/article.js
+++ backend/models/article.js
@@ -2,9 +2,27 @@
 const mongoose = require('mongoose')
 
 const articleSchema = new mongoose.Schema({
-  title: {
+  title : {
+    type : String,
+    required : true,
+    unique : true
+  },
+  content: {
     type: String,
-    required: true,
-    unique: true
+    required: true  
+  },
+  image: {
+    type: String,
+    required: false
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  categories: {
+    type: Array,
+    required: true
   }
 })
+
+module.exports = mongoose.model('Article', articleSchema)
